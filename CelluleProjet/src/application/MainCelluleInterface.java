9041213ae01886057d5eb5f1e9fd2cfc1043038e
ST3VOS <ST3VOS@DESKTOP-4MGTQCB>
2020-12-17 13:20:37 +0100

    Commit Test 2

--- CelluleProjet/src/application/MainCelluleInterface.java
+++ CelluleProjet/src/application/MainCelluleInterface.java
@@ -21,7 +21,7 @@ public class MainCelluleInterface extends Application {
 			e.printStackTrace();
 		}
 	}
-	// Test commit beta
+	// Test commit beta 2
 	public static void main(String[] args) {
 		launch(args);
 	}
